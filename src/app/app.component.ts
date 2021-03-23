2cd1c7033eaaebad58d52f029b2a97397c96f6bc
bandilemashile <35614625+bandilemashile@users.noreply.github.com>
2021-03-23 17:32:26 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 
+
+
+
+export class Name {
+  constructor(
+
+  public name : string
+ 
+
+  ){
+
+  }
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-app';
+  name:Name[];
+
+  constructor(
+    private httpClient : HttpClient
+  ){
+
+  }
+
+  ngOnInit():void{
+   this.getName();
+  }
+
+
+  getName(){
+    this.httpClient.get<any>('http://localhost:3000/').subscribe(
+    response => {
+      console.log(response);
+      this.name = response;
+    }
+    );
+  }
 }
+
+
